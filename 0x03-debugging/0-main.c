fc837985affe339dad03cd8d1dcb1ad1fbbcf325
Justin Okolo <justinokolo68@gmail.com>
2022-10-20 13:17:55 -0700

    task again

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * main - Test function fo rnegative or positive
+ * main - test function for positive or negative number
  * Return: 0
+ *
  */
 
 int main(void)
@@ -12,5 +13,5 @@ int i;
 i = 0;
 positive_or_negative(i);
 
-return (0)
+return(0);
 }
